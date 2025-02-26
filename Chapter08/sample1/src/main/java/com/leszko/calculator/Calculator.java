7466bf2fbf1f04f60e8791f6a9d6b84358782383
Michael Morse <morsema88@gmail.com>
2025-02-26 17:02:57 -0500

    Fix: Add Javadoc comments to pass Checkstyle validation

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -1,14 +1,26 @@
 package com.leszko.calculator;
-// This is a test change for the PR to master branch
 
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+/**
+ * The Calculator class provides basic arithmetic operations.
+ * It includes a caching mechanism for the sum operation.
+ */
 @Service
 public class Calculator {
-        final static int umlNUMBER1 = 3;
-	@Cacheable("sum")
-	public int sum(int a, int b) {
-		return a + b;
-	}
+    final static int umlNUMBER1 = 3;
+
+    /**
+     * Returns the sum of two numbers.
+     * 
+     * @param a First number
+     * @param b Second number
+     * @return Sum of a and b
+     */
+    @Cacheable("sum")
+    public int sum(int a, int b) {
+        return a + b;
+    }
 }
+
