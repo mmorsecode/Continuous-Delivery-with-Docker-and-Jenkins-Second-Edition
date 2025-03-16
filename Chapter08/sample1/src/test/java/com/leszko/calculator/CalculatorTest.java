7f6c4b4e1513fe65d61bf131bb642c6f2f5b8a3f
Michael Morse <morsema88@gmail.com>
2025-03-16 17:41:58 -0400

    Test: Force a failure to verify Jenkins failure handling

--- Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
+++ Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
@@ -5,8 +5,9 @@ import static org.junit.Assert.assertEquals;
 public class CalculatorTest {
      private Calculator calculator = new Calculator();
 
-     @Test
-     public void testSum() {
-          assertEquals(5, calculator.sum(2, 3));
-     }
+	@Test
+	public void testSum() {
+    		Calculator calculator = new Calculator();
+    		assertEquals(5, calculator.sum(2, 2)); // Change expected result to force failure
 }
+
